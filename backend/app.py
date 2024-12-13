e2ee86ddb2f1555da1cfb264f81174aa71f4b53b
TingZhang <tingzhang@mail.spstaiwan.com.tw>
2024-12-13 16:21:10 +0800

    fix: before nuxt

--- backend/app.py
+++ backend/app.py
@@ -48,10 +48,10 @@ MONGO_USER = str(os.getenv("MONGODB_USERNAME"))
 MONGO_PASS = str(os.getenv("MONGODB_PASSWORD"))
 SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60*24
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 jwt_instance = JWT()
-jwk = OctetJWK(SECRET_KEY.encode('utf-8'))
+jwk = OctetJWK(SECRET_KEY.encode("utf-8"))
 
 # 連接MongoDB
 if MONGO_HOST == "localhost":
@@ -119,8 +119,9 @@ def register(user: model.UserRegisterItem):
 
     # 使用pbkdf2对密码进行加密，使用随机生成的salt
     hashed_password = hashlib.pbkdf2_hmac(
-        'sha256', user.password.encode('utf-8'), salt, 100000)
-    
+        "sha256", user.password.encode("utf-8"), salt, 100000
+    )
+
     # 创建新用户，存储salt和密码哈希
     user = {
         "username": user.username,
@@ -128,18 +129,18 @@ def register(user: model.UserRegisterItem):
         "salt": salt.hex(),
         "email": user.email,
         "email_verified": False,
-        'role': 'user',
-        'group': [],
-        'user_data_session': {
-            'register_time': datetime.now(),
-            'last_login_time': datetime.now(),
-            'last_password_change_time': datetime.now(),
+        "role": "user",
+        "group": [],
+        "user_data_session": {
+            "register_time": datetime.now(),
+            "last_login_time": datetime.now(),
+            "last_password_change_time": datetime.now(),
         },
-        'session': {
-            'token': '',
-            'refresh_token': '',
-            'token_type': 'bearer',
-        }
+        "session": {
+            "token": "",
+            "refresh_token": "",
+            "token_type": "bearer",
+        },
     }
     user_result = db.users.insert_one(user)
     return {"success": True, "message": "注册成功"}
@@ -149,39 +150,54 @@ def register(user: model.UserRegisterItem):
 def login(login_item: model.UserLoginItem):
     user = db.users.find_one({"username": login_item.username})
     if not user:
-        raise HTTPException(status_code=400, detail="用户名或密码错误")
+        raise HTTPException(status_code=400, detail="帳號或密碼錯誤")
     salt = bytes.fromhex(user["salt"])
     hashed_password = hashlib.pbkdf2_hmac(
-        'sha256', login_item.password.encode('utf-8'), salt, 100000)
+        "sha256", login_item.password.encode("utf-8"), salt, 100000
+    )
     if hashed_password.hex() != user["password"]:
-        raise HTTPException(status_code=400, detail="用户名或密码错误")
+        raise HTTPException(status_code=400, detail="帳號或密碼錯誤")
     access_token = create_access_token(data={"sub": login_item.username})
     refresh_token = create_refresh_token(data={"sub": login_item.username})
-    db.users.update_one({"username": login_item.username}, {
-                        "$set": {"session.token": access_token, "session.refresh_token": refresh_token}})
+    db.users.update_one(
+        {"username": login_item.username},
+        {
+            "$set": {
+                "session.token": access_token,
+                "session.refresh_token": refresh_token,
+            }
+        },
+    )
     return_data = {
-        'user_id': str(user["_id"]),
-        'user_name': user["username"],
-        'email': user["email"],
-        'user_data': user["user_data"],
-        'user_data_session': user["user_data_session"],
+        "user_id": str(user["_id"]),
+        "user_name": user["username"],
+        "email": user["email"],
+        "user_data": user["user_data"],
+        "user_data_session": user["user_data_session"],
     }
-    return {"success": True, "message": "登录成功", "token": access_token, "refresh_token": refresh_token, "token_type": "bearer", "user_data": return_data}
+    return {
+        "success": True,
+        "message": "登录成功",
+        "token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer",
+        "user_data": return_data,
+    }
 
 
-@app.post('/get_role_by_token')
+@app.post("/get_role_by_token")
 def get_role_by_token(token: model.TokenModel):
     payload = decode_access_token(token.token)
     print("payload", payload)
-    if payload['exp'] < datetime.now().timestamp():
+    if payload["exp"] < datetime.now().timestamp():
         raise HTTPException(status_code=401, detail="Token expired")
-    user = db.users.find_one({'session.token': token.token})
+    user = db.users.find_one({"session.token": token.token})
     return_data = {
-        'user_name': user["username"],
-        'email': user["email"],
-        'user_id': str(user["_id"]),
-        'user_data': user["user_data"],
-        'user_data_session': user["user_data_session"],
+        "user_name": user["username"],
+        "email": user["email"],
+        "user_id": str(user["_id"]),
+        "user_data": user["user_data"],
+        "user_data_session": user["user_data_session"],
     }
     return {"success": True, "message": "获取角色成功", "user_data": return_data}
 
@@ -189,33 +205,47 @@ def get_role_by_token(token: model.TokenModel):
 @app.post("/add_spending")
 def add_spending(spending: model.SpendingItem):
     print(spending)
-    return {"time": spending.time, "amount": spending.amount, "type": spending.type, "description": spending.description}
+    return {
+        "time": spending.time,
+        "amount": spending.amount,
+        "type": spending.type,
+        "description": spending.description,
+    }
+
 
 @app.post("/add_group")
 def add_group(add_group: model.AddGroupItem):
-    default_type = db['web_status'].find_one({"name":"default_type"})
-    default_fast_input = db['web_status'].find_one({"name":"default_fast_input"})
+    default_type = db["web_status"].find_one({"name": "default_type"})
+    default_fast_input = db["web_status"].find_one({"name": "default_fast_input"})
     upload_data = {
         "group_name": add_group.group_name,
         "type": default_type["value"],
         "fast_input": default_fast_input["value"],
         "member": [add_group.user_id],
     }
-    db['group'].insert_one(upload_data)
+    db["group"].insert_one(upload_data)
     return {"success": True, "message": "新增群組成功"}
+
 
 @app.post("/get_group")
 def get_group(get_group: model.GetGroupItem):
-    group_list = list(db['group'].find({"member": {"$in": [get_group.user_id]}}))
+    group_list = list(db["group"].find({"member": {"$in": [get_group.user_id]}}))
     for group in group_list:
-        group['_id'] = str(group['_id'])
+        group["_id"] = str(group["_id"])
     return {"success": True, "message": "獲取群組成功", "data": group_list}
+
 
 @app.post("/get_user_list_by_group")
 def get_user_list_by_group(get_user_list_by_group: model.GetUserListByGroupItem):
     print(get_user_list_by_group)
-    group_data = db['group'].find_one({"_id": ObjectId(get_user_list_by_group.group_id)})
+    group_data = db["group"].find_one(
+        {"_id": ObjectId(get_user_list_by_group.group_id)}
+    )
     if group_data is not None:
-        return {"success": True, "message": "獲取群組成員成功", "data":group_data["member"]}
+        return {
+            "success": True,
+            "message": "獲取群組成員成功",
+            "data": group_data["member"],
+        }
     else:
         return {"success": False, "message": "群組不存在"}
